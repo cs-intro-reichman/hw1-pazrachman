059b412cd2dfe14c631a8d521cbac3f1598901c8
pazrachman <paz.rachman@post.runi.ac.il>
2024-11-07 18:37:24 +0200

    ;

--- FVCalc.java
+++ FVCalc.java
@@ -7,7 +7,7 @@ public class FVCalc {
 	int year = Integer.parseInt(args[2]);
 	int futureValue = (int)(money * Math.pow(rate/100 + 1, year));
 
-	System.out.println("After " + year + " years, " + "$" + money + " " + "saved " + "at " + rate + "% " + "will " + "yeild " + "$" + futureValue );
+	System.out.println("After " + year + " years, " + "$" + money + " " + "saved " + "at " + rate + "% " + "will " + "yield " + "$" + futureValue );
 
 
 	}
