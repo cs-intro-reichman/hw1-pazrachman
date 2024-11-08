fb4834a11168e127f9b99741ef3cdbbca3af1236
pazrachman <paz.rachman@post.runi.ac.il>
2024-11-08 18:52:03 +0200

    l

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,11 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int NumWords = Integer.parseInt(args[0]);
+		int ones = NumWords%10;
+		int tens = (NumWords - ones) %100/10;
+		int hundreds = NumWords/100;
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones." );
+		
 	}
 }
