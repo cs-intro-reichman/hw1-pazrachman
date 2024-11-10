27c376a029b6279e3cf40257cbcdfa8d993d9b46
pazrachman <paz.rachman@post.runi.ac.il>
2024-11-10 17:51:07 +0200

    kk;

--- Ascend.java
+++ Ascend.java
@@ -1,14 +1,20 @@
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
+
+import java.util.Random;
+
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
-		int num = 200;
-		int num2 = 40;
-		int num3 = 10;
+	int lim = Integer.parseInt(args[0]);
+	int a = (int)(Math.random() * lim);
+	int b = (int)(Math.random() * lim);
+	int c = (int)(Math.random() * lim);
 
-		int sum = num + num2 / num3;
-
-		System.out.println(sum);
+	int max = Math.max(Math.max(a, b), c);
+	int min = Math.min(Math.min(a, b), c);
+	int mid = a + b + c - (max + min);
+	System.out.println(a + " " + b + " " + c);
+	System.out.println(min + " " + mid + " " + max);
+	
 	}
 }
